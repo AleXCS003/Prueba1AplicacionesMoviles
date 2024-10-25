307980a126e1f964d9bed620e8d89077884a2ed1
AleXCS003 <alexd003cardenas@gmail.com>
2024-10-25 14:42:23 -0500

    respaldo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Gracias por visitar mi biografía. Siempre estoy abierto a nuevas oportunidades.Gracias por visitar mi biografía. Siempre estoy abierto a nuevas oportunidades.',
+      duration: 1500,
+      position: position,
+    });
+    
+
+    await toast.present();
+
+  }
+  alertButtons = ['Action'];
 }
