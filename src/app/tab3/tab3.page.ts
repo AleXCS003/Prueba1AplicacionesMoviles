6ddd793624e6f3605ff9c0e3c17b5a7b538c2002
AleXCS003 <alexd003cardenas@gmail.com>
2024-10-25 14:57:28 -0500

    respaldo

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  startDate: string;
+  endDate: string;
+  daysDifference: number | null = null;
 
-  constructor() {}
+  constructor() {
+    // Inicializar las fechas con un valor por defecto si es necesario
+    this.startDate = '';
+    this.endDate = '';
+  }
 
+  calculateDays() {
+    if (this.startDate && this.endDate) {
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+      const timeDifference = end.getTime() - start.getTime();
+      this.daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24)); // Convertir de milisegundos a días
+    } else {
+      this.daysDifference = null; // Resetear si alguna fecha no está definida
+    }
+  }
+  
 }
